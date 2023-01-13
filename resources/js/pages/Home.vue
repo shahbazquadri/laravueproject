11f7913dc12ebd7fa7504fb2dc6d4bd8d24e0075
shahbazquadri <sbzquadri@gmail.com>
2023-01-13 08:59:50 +0530

    hashing method added

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -298,6 +298,22 @@ export default {
             this.errorinform = [];
             var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
+            /****************Hasing***************/
+            function sha512(str) {
+                return crypto.subtle
+                    .digest("SHA-512", new TextEncoder("utf-8").encode(str))
+                    .then((buf) => {
+                        return Array.prototype.map
+                            .call(new Uint8Array(buf), (x) =>
+                                ("00" + x.toString(16)).slice(-2)
+                            )
+                            .join("");
+                    });
+            }
+
+            sha512("my string for hashing").then((x) => console.log(x));
+
+            /************END*************/
             if (this.applicantname.trim() == "") {
                 this.errorinform.push("Please enter Applicant name");
                 // alert("Please enter Applicant name");
